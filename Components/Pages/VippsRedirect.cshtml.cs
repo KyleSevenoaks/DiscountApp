4ed70147faffc0a3a149cd3784446d2001d5f67b
KyleSevenoaks <KyleSevenoaks@users.noreply.github.com>
2025-10-24 14:04:25 +0200

    Adds summary

---
+++ Components/Pages/VippsRedirect.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DiscountApp.Components.Pages.Pages;
+
+public class VippsRedirectModel : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
