e194b872160e7fa80c01cf39a17823c3144c0269
KyleSevenoaks <kylesevenoaks@gmail.com>
2025-10-25 11:00:32 +0200

    adds prices to discount cards

--- obj/Debug/net9.0/DiscountApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/DiscountApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiscountApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+853ee3b7d3c4a664af1a6080cf273b81a791ea70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35535f90c6b28cd51966ff5fc60d1f765cb82ed3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiscountApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiscountApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
